93f099d9cab8199b013eeccf9168b0e6807ce0a6
dadalee51 <dadalee51@gmail.com>
2020-06-24 15:24:33 +1000

    added micropython works

--- lesson6.py
+++ lesson6.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #testing motor
 import ev3dev2.motor as em
+import time as t
 m1= em.LargeMotor(em.OUTPUT_A)
 m2= em.LargeMotor(em.OUTPUT_B)
 m3= em.LargeMotor(em.OUTPUT_C)
@@ -9,6 +10,10 @@ m1.on(em.SpeedPercent(25))
 m2.on(em.SpeedPercent(25))
 m3.on(em.SpeedPercent(25))
 m4.on(em.SpeedPercen(25))
-
+t.sleep(1)
+m1.off()
+m2.off()
+m3.off()
+m4.off()
 
 
